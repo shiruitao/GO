4720a926ed58bc064277dd67733b36acc47566aa
shitime <1257201113@qq.com>
2017-11-29 20:56:11 +0800

    迷惘?

---
+++ CCC.go
@@ -0,0 +1,26 @@
+/*
+* Shi Ruitao  17-11-28
+*/
+
+package main
+import "fmt"
+func getSequence() func() int {
+	i := 0
+	return func() int {
+		i ++
+		return i
+	}
+}
+
+func main() {
+	nextNumber := getSequence()
+	fmt.Println(nextNumber())
+	fmt.Println(nextNumber())
+	fmt.Println(nextNumber())
+	fmt.Println(nextNumber())
+	fmt.Println(nextNumber())
+	nextNumber1 := getSequence()
+	fmt.Println(nextNumber1())
+	fmt.Println(nextNumber1())
+	fmt.Println(nextNumber1())
+}
