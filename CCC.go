358fef67f0d74e0aab0344480634ac5f560a99d0
shitime <1257201113@qq.com>
2017-11-30 21:26:19 +0800

    go

--- CCC.go
+++ CCC.go
@@ -12,24 +12,26 @@ func getSequence() func() int {
 	}
 }
 
+func GetAverage(arr []int, size int) float32 {
+	var i, sum int
+
+	for i = 0; i < size; i++ {
+		sum += arr[i]
+	}
+	return float32(sum / size)
+}
+
 func main() {
 	nextNumber := getSequence()
 	fmt.Println(nextNumber())
-	fmt.Println(nextNumber())
-	fmt.Println(nextNumber())
-	fmt.Println(nextNumber())
-	fmt.Println(nextNumber())
 	nextNumber1 := getSequence()
 	fmt.Println(nextNumber1())
-	fmt.Println(nextNumber1())
-	fmt.Println(nextNumber1())
-	
 	arr := [3] int {1, 2, 3}
 	fmt.Println(arr)
 
-	var arr1 [2] int
-	// arr1[0] = 5
-	// arr1[1] = 6
-	arr1 = [2] int {1, 2}
-	fmt.Println(arr1)
+	var arr1 = [] int {1, 2, 13}
+	var avg float32
+	avg = getAverage(arr1, 4)
+	fmt.Printf("avg = %f \n", avg)
+	fmt.Printf("地址 %x \n", &avg)
 }
