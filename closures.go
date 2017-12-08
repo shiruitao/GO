1ed02d6b7ee32470c16f345d7d8ed3e27e03ec2e
shitime <1257201113@qq.com>
2017-12-08 14:49:42 +0800

    go

---
+++ closures.go
@@ -0,0 +1,36 @@
+/*
+* Shi Ruitao 2017-12-8
+*/
+
+package main
+import "fmt"
+
+func closed() func() func() int {
+	i := 0
+	return func() func() int {
+		i += 1
+		return func() int {
+			i += 1
+			return i
+		}
+	}
+}
+
+func main() {
+	fun := closed()
+	// fun1 := fun()
+	fmt.Println(fun()())
+	fmt.Println(fun()())
+	fmt.Println(fun()())
+	fmt.Println(fun()())
+	fmt.Println(fun()())
+
+	fun = closed()
+	fun1 := fun()
+	fmt.Println(fun1())
+	fmt.Println(fun1())
+	fmt.Println(fun1())
+	fmt.Println(fun1())
+	fmt.Println(fun1())
+	
+}
