496bdcd4eb15c18017cf4f9bd35dcaf4170cd992
shitime <1257201113@qq.com>
2018-01-02 21:03:21 +0800

    copy

--- copy.go
+++ copy.go
@@ -5,7 +5,7 @@ import (
 
 func main() {
 
-var a = []int{0, 1, 2, 3, 4, 5, 6, 7}
+var a = []int{0, 1, 2}
 
 var s = make([]int, 6)
 
@@ -15,23 +15,10 @@ fmt.Println("s - ", s)
 fmt.Println("n1 - ", n1)
 
 // 源长为7,目标为6,复制索引1到6
-n2 := copy(s, a[1:])
-
-fmt.Println("s - ", s)
-
-fmt.Println("n2 - ", n2)
-
-// 源长为8-5=3,只会复制3个值,目标中的后三个值不会变
-
-n3 := copy(s, a[5:])
-
-fmt.Println("s - ", s)
-
-fmt.Println("n3 - ", n3)
 
 // 将源中的索引5,6,7复制到目标中的索引3,4,5
-n4 := copy(s[3:], a[5:])
-fmt.Println("s - ", s)
-fmt.Println("n4 - ", n4)
+copy(a, a[:2])
+fmt.Println(cap(a))
+fmt.Println("最后", a)
 
 }
