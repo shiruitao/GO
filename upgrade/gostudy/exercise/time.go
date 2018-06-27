3c2cf76010f857f28ec4bc12506490a9a09ccef0
shiruitao <1257201113@qq.com>
2018-06-27 23:32:59 +0800

    go

--- upgrade/gostudy/exercise/time.go
+++ upgrade/gostudy/exercise/time.go
@@ -36,6 +36,6 @@ import (
 
 func Time() {
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
-	const maxInt = int(^uint(0) >> 1)
-	fmt.Println(maxInt)
+	//const maxInt = int(^uint(0) >> 1)
+	//fmt.Println(maxInt)
 }
